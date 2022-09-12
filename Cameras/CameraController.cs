f91378034d07b490141c451f77246cdd5457bfb4
Brian Hungerman <bitnaughts@gmail.com>
2022-09-12 02:37:42 -0700

    Up to date vocals

--- Cameras/CameraController.cs
+++ Cameras/CameraController.cs
@@ -16,9 +16,12 @@ public class CameraController : MonoBehaviour {
     GameObject cursor;
     GameObject example;
 
+    public Interactor Interactor;
     public OverlayInteractor OverlayInteractor;
 
     float speed = 1f;
+    Vector3 panOrigin;
+    bool bDragging;
 
     void Start() {
 
@@ -97,53 +100,68 @@ public class CameraController : MonoBehaviour {
     //         this.GetComponent<Camera>().orthographicSize = 140f + (int)((timer-120) * 1) * 20;
     //         focus.GetComponent<AudioSource>().volume = Mathf.Lerp(0.5f, 0f, (timer - 120)/15f);
     //     }
-    //     if (Input.mousePosition.x > Screen.width / 2) //&& Input.mousePosition.x < 3 * Screen.width / 4 )  
-    //     {
-    //         if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-    //             if (GameObject.Find("Dropdown List") == null) { // && EventSystem.current.currentSelectedGameObject == null
-    //                 GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize - Input.GetAxis("Mouse ScrollWheel") * GetComponent<Camera>().orthographicSize / 10, 6f, 800f);
-    //                 if (OverlayInteractor.gameObject.activeSelf) OverlayInteractor.Resize();
-    //             }
-    //         }
-    //         if (Input.touchCount == 2) {
-    //             Vector2[] newPositions = new Vector2[]{Input.GetTouch(0).position, Input.GetTouch(1).position};
-    //             if (!wasZoomingLastFrame) {
-    //                 wasZoomingLastFrame = true;
-    //                 lastZoomPositions = newPositions;
-    //             } else {
-    //                 float offset = Vector2.Distance(newPositions[0], newPositions[1]) - Vector2.Distance(lastZoomPositions[0], lastZoomPositions[1]);
-    //                 this.GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize - offset, 6f, 800f);
-    //                 if (OverlayInteractor.gameObject.activeSelf) OverlayInteractor.Resize();
-    //                 lastZoomPositions = newPositions;
-    //             }
-    //         } else {
-    //             wasZoomingLastFrame = false;
-    //         }
-    //         if (Input.GetMouseButtonDown(0) && OverlayInteractor.gameObject.activeSelf == false) { // && EventSystem.current.currentSelectedGameObject == null
-    //             dragOrigin = this.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition);
-    //             cursor.SetActive(true);
-    //             cursor.transform.position = Input.mousePosition;
-                
-    //             oldPos = transform.position;
-    //             return;
-    //         }
-    //     }
-    //     if (!Input.GetMouseButton(0)) 
-    //     {
-    //         cursor.SetActive(false);
-    //         return;
-    //     }
+        if (Input.mousePosition.x > Screen.width / 2) //&& Input.mousePosition.x < 3 * Screen.width / 4 )  
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+                if (GameObject.Find("Dropdown List") == null) { // && EventSystem.current.currentSelectedGameObject == null
+                    GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize - Input.GetAxis("Mouse ScrollWheel") * GetComponent<Camera>().orthographicSize / 10, 6f, 800f);
+                    if (OverlayInteractor.gameObject.activeSelf) OverlayInteractor.Resize();
+                    
+                    Interactor.PanTutorial();
+                }
+            }
+            if (Input.touchCount == 2) {
+                Vector2[] newPositions = new Vector2[]{Input.GetTouch(0).position, Input.GetTouch(1).position};
+                if (!wasZoomingLastFrame) {
+                    wasZoomingLastFrame = true;
+                    lastZoomPositions = newPositions;
+                } else {
+                    float offset = Vector2.Distance(newPositions[0], newPositions[1]) - Vector2.Distance(lastZoomPositions[0], lastZoomPositions[1]);
+                    this.GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize - offset, 6f, 800f);
+                    if (OverlayInteractor.gameObject.activeSelf) OverlayInteractor.Resize();
+                    lastZoomPositions = newPositions;
+                    Interactor.PanTutorial();
+                }
+            } else {
+                wasZoomingLastFrame = false;
+            }
+        }
+        if(Input.GetMouseButtonDown(0) && OverlayInteractor.gameObject.activeSelf == false && Input.mousePosition.x > Screen.width / 2)
+        {
+            cursor.SetActive(true);
+            bDragging = true;
+            oldPos = transform.position;
+            //Get the ScreenVector the mouse clicked
+            //https://answers.unity.com/questions/827834/click-and-drag-camera.html#:~:text=Click%20and%20Drag%20Camera%20-%20Unity%20Answers%20void,%2F%2FGet%20the%20ScreenVector%20the%20mouse%20clicked%20%7D%20if%28Input.GetMouseButton%280%29%29
+            panOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if(Input.GetMouseButton(0) && OverlayInteractor.gameObject.activeSelf == false && bDragging)
+        {
+            cursor.transform.position = this.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition);
+            //Get the difference between where the mouse clicked and where it moved
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - panOrigin;    
+            //Move the position of the camera to simulate a drag, speed * 10 for screen to worldspace conversion
+            transform.position = oldPos + -pos * GetComponent<Camera>().orthographicSize;   
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10f);          
+        }
+        if(Input.GetMouseButtonUp(0) && bDragging)
+        {
+            Interactor.PanTutorial();
+            bDragging = false;
+        }
 
-    //     // Vector3 change = Input.mousePosition - dragOrigin;
-    //     // change.x += Screen.width / 2;
-    //     // Vector3 pos = this.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition) - dragOrigin;
-    //     // if (cursor.activeSelf && !OverlayInteractor.gameObject.activeSelf) {
-    //     //     // pos.x *= dragSpeed;
-    //     //     // pos.y *= dragSpeed;   
-    //     //     transform.position = oldPos + -pos * GetComponent<Camera>().orthographicSize * 2f;
-    //     // }
-
-    //     // dragOrigin = Input.mousePosition;
+        // // Vector3 change = Input.mousePosition - dragOrigin;
+        // // change.x += Screen.width / 2;
+        // // Vector3 pos = this.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition) - dragOrigin; this.GetComponent<Camera>().ScreenToViewportPoint(
+        // // print (pos);
+        // if (cursor.activeSelf && !OverlayInteractor.gameObject.activeSelf) {
+        //     // pos.x *= dragSpeed;
+        //     // pos.y *= dragSpeed;   
+        //     transform.position = this.GetComponent<Camera>().ScreenToViewportPoint(oldPos + Input.mousePosition);// * GetComponent<Camera>().orthographicSize * 2f;
+        //     transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+        // }
+        // // oldPos = Input.mousePosition;
+        // dragOrigin = Input.mousePosition;
     }
 
     public void ZoomIn() {
